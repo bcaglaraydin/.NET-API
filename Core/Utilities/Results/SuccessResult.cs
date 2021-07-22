fd179da32881a8175ad20574928e09191093b0a8
bcaglaraydin <berdancaglaraydin@gmail.com>
2021-07-22 15:12:27 +0300

    22.07.2021

--- Core/Utilities/Results/SuccessResult.cs
+++ Core/Utilities/Results/SuccessResult.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Core.Utilities.Results
 {
-    public class SuccessResult:Result
+    public class SuccessResult : Result
     {
-        public SuccessResult(string message):base(true, message)
+        public SuccessResult(string message) : base(true, message)
         {
 
         }
