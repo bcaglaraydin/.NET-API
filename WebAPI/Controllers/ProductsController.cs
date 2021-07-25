40e216e91713216da0ef568c200f630782df4450
bcaglaraydin <berdancaglaraydin@gmail.com>
2021-07-25 20:25:43 +0300

    WebAPI - Autofac

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -27,8 +27,7 @@ namespace WebAPI.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
-           
-            
+    
             var result = _productService.GetAll();
             if (result.Success)
             {
